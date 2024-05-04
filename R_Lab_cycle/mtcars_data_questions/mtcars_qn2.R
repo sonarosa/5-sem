f951bd3863174f75004c16f7ff74864ff1ac52e2
sonarosa <sonarosababloo@gmail.com>
2024-05-04 22:29:03 +0530

    app updated code

--- R_Lab_cycle/mtcars_data_questions/mtcars_qn2.R
+++ R_Lab_cycle/mtcars_data_questions/mtcars_qn2.R
@@ -1,16 +1,14 @@
 # Load the mtcars dataset from the mtcars.csv file
 mtcars_data <- read.csv("mtcars.csv")
 
-# Create a bar plot with different colors for "am"
+# Create a bar plot of the number of cylinders with different colors for "am"
 barplot(table(mtcars_data$cyl, mtcars_data$am),
         beside = TRUE,
-        col = c("blue", "red"),
-        main = "Bar Plot of Cylinders by Transmission Type",
-        xlab = "Cylinders",
-        ylab = "Frequency",
-        names.arg = unique(mtcars_data$cyl),
-        legend.text = unique(mtcars_data$am),
-        args.legend = list(title = "Transmission Type", x = "topright"))
+        col = c("red", "blue"),
+        xlab = "Number of Cylinders",
+        ylab = "Count",
+        main = "Number of Cylinders by Transmission Type",
+        names.arg = unique(mtcars_data$cyl))
 
-# Adding a legend
-legend("topright", legend = unique(mtcars_data$am), fill = c("blue", "red"), title = "Transmission Type")
+# Add a legend
+legend("topright", legend = unique(mtcars_data$am), fill = c("red", "blue"), title = "Transmission Type")
